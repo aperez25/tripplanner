eacbf8ac22a4213cac1653762f75a1fa03147870
aperez25 <alice.perez25@gmail.com>
2017-05-09 17:15:12 -0700

    simplified index calls

--- public/js/itinerary.js
+++ public/js/itinerary.js
@@ -1,43 +1,32 @@
 
-const delButton = '<button class="btn btn-xs btn-danger remove btn-circle id="">x</button>'
+const delButton = '<button class="btn btn-xs btn-danger remove btn-circle id="">x</button>',
+  itineraryMarkers = [];
 
 $('#hotel-add').on('click', function(){
-  // capture selected hotel value
-  const $hotelSelected = $('#hotel-choices').children('option:selected').text();
+  // capture selected hotel value & index
+  const $hotelSelected = $hotelChoices.children('option:selected').text();
+  const $hotelIndex = $hotelChoices.children('option:selected').index();
+  // creat Google map marker
+  const marker = drawMarker('hotel', hotels[$hotelIndex].place.location);
   // add hotel to the itinerary list
   $('#hotel-item').append('<span class="itinerary">' + $hotelSelected + ' ' + delButton + '</span>');
-  // find the index of the hotel in hotels array
-  const index = hotels.findIndex(function(hotel){
-    return hotel.name ===  $hotelSelected;
-  });
-  drawMarker('hotel', hotels[index].place.location);
 });
 
 $('#restaurant-add').on('click', function(){
-  // capture selected restaurant value
-  const $restaurantSelected = $('#restaurant-choices').children('option:selected').text();
-  // add restaurant to the itinerary list
-  $('#restaurant-item').append('<span class="itinerary">' + $restaurantSelected  + ' ' + delButton + '</span>' );
-  // find the index of the restaurant in restaurants array
-  const index = restaurants.findIndex(function(restaurant){
-    return restaurant.name ===  $restaurantSelected;
-  });
-  drawMarker('restaurant', restaurants[index].place.location);
+  const $restaurantSelected = $restaurantChoices.children('option:selected').text();
+  const $restaurantIndex = $restaurantChoices.children('option:selected').index();
+  const marker = drawMarker('restaurant', restaurants[$restaurantIndex].place.location);
+  // itineraryMarkers.push(restaurants[$restaurantIndex].place.location);
+  $('#restaurant-item').append('<span class="itinerary">' + $restaurantSelected  + ' ' + delButton + '</span>');
 });
 
 $('#activity-add').on('click', function(){
   // capture selected activities value
-  const $activitiesSelected = $('#activity-choices').children('option:selected').text();
-  const $activitiesIndex = $('#activity-choices').children('option:selected').index
-  // find the index of the activities in activitiess array
-  const index = activities.findIndex(function(activities){
-    return activities.name ===  $activitiesSelected;
-  });
-  const marker = drawMarker('activity', activities[index].place.location);
-  console.log(marker.position);
+  const $activitiesSelected = $activityChoices.children('option:selected').text();
+  const $activitiesIndex = $activityChoices.children('option:selected').index();
+  const marker = drawMarker('activity', activities[$activitiesIndex].place.location);
   // add activities to the itinerary list
-  $('#activity-item').append('<span class="itinerary" id="' + $activitiesIndex + '">' + $activitiesSelected + ' ' + delButton + '</span>').data();
-
+  $('#activity-item').append('<span class="itinerary">' + $activitiesSelected + ' ' + delButton + '</span>');
 });
 
 $('#itinerary').on('click', '.remove', function(){
